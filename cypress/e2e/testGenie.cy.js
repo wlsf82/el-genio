0d36c45d9a579ef7da0be31a7156d07ae3767c6a
Walmyr <wlsf82@gmail.com>
2025-04-02 21:00:31 +0200

    Add extra assertions on test

--- cypress/e2e/testGenie.cy.js
+++ cypress/e2e/testGenie.cy.js
@@ -23,22 +23,31 @@ describe('TestGenie', () => {
     // Add test case description
     cy.get('input[placeholder="Enter test case description"]').type('asserts h1 is visible')
 
-    // Add steps
+    // Add steps and assert on their previews
     cy.get('select').select('visit')
     cy.get('input[placeholder="Value"]').type('https://walmyr.dev')
     cy.contains('button', 'Add step').click()
+    cy.contains('.steps-list', 'visit "https://walmyr.dev"').should('be.visible')
     cy.get('select').select('contains')
     cy.get('input[placeholder="Selector (e.g., #button, .class)"]').type('h1')
     cy.get('input[placeholder="Value"]').type('Walmyr')
     cy.contains('button', 'Add step').click()
+    cy.contains('.steps-list', 'get element with selector "h1" which contains "Walmyr"').should('be.visible')
     cy.get('select').select('should')
     cy.contains('option', 'Select an assertion')
       .parent()
       .select('be.visible')
     cy.contains('button', 'Add step').click()
+    cy.contains('.steps-list', 'asserts it should "be.visible"').should('be.visible')
 
     // Add test case
     cy.contains('button', 'Add test case').click()
+
+    // Assert test case preview is displayed
+    cy.contains('.test-cases-list', 'asserts h1 is visible').should('be.visible')
+    cy.contains('.test-cases-list', 'visit "https://walmyr.dev"').should('be.visible')
+    cy.contains('.test-cases-list', 'get element with selector "h1" which contains "Walmyr"').should('be.visible')
+    cy.contains('.test-cases-list', 'asserts it should "be.visible"').should('be.visible')
 
     // Create test suite
     cy.contains('button', 'Create test suite').click()
