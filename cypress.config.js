6cd8ba11b7d822c39147d5ad19f07711aaf70b82
Walmyr <wlsf82@gmail.com>
2025-04-04 00:41:11 +0200

    Remove function not in use
    And use single quotes for string to keep the consistency.

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
