5f35cc89d186347ddc65127c63190e3d98eb1d4b
Walmyr <wlsf82@gmail.com>
2025-04-08 23:56:17 +0200

    Update cypress default config
    When the server is running on Docker, it has chromium installed,
    and so, when running on linux, we use the chromium browser, other-
    wise, we use chrome.

--- server/config/cypressConfig.js
+++ server/config/cypressConfig.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
+const browser = process.platform === 'linux' ? 'chromium' : 'chrome';
+
 // Define default Cypress options
 const defaultCypressOptions = {
-  browser: 'chromium',
+  browser,
   headed: false,
   configFile: path.join(__dirname, '../', 'cypress.config.js'),
 };
