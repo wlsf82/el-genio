992a95fb37d82bf277a9478b58623789c5ccfd02
Walmyr <wlsf82@gmail.com>
2025-04-17 03:26:23 +0200

    Increase viewport for tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    viewportHeight: 1024,
+    viewportWidth: 1280,
   },
 });
