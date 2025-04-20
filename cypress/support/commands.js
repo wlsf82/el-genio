07dd33b4fbcee9829504caa93a94dad013c75e71
Walmyr <wlsf82@gmail.com>
2025-04-21 01:12:38 +0200

    Delete not used custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,13 +44,3 @@ Cypress.Commands.add('deleteProjectByName', name => {
       })
     })
 })
-
-Cypress.Commands.add('deleteTestSuitesByName', name => {
-  cy.request('GET', '/api/test-suites')
-    .then((response) => {
-      const testSuitesToDelete = response.body.filter(suite => suite.name === name)
-      testSuitesToDelete.forEach(suite => {
-        cy.request('DELETE', `/api/test-suites/${suite.id}`)
-      })
-    })
-})
