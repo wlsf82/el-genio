5d3cdebacd75ccd648ec46e5e3c09fde64faa675
Gabriel Neutzling <gabrielneutzling@gmail.com>
2025-05-04 19:57:46 -0400

    add integration to project list

--- webapp/app/projects/page.tsx
+++ webapp/app/projects/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
+import React from "react";
 import { AppHeader } from "@/components/app-header";
 import { PageTitle } from "@/components/page-title";
 import { ProjectsGrid } from "@/components/projects-grid";
 import { RecentExecutions } from "@/components/recent-executions";
 import Link from "next/link";
+import { fetchProjects } from "@/services/projects";
+import { Project } from "@/types/projects";
 
 export default function ProjectsPage() {
+  const [projects, setProjects] = React.useState<Project[]>([]);
+
+  const fetchProjectsData = async () => {
+    try {
+      const response = await fetchProjects();
+      setProjects(response);
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchProjectsData();
+  }, []);
+
+  console.log(projects);
+
   return (
     <div className="flex flex-col h-screen bg-accent">
       <AppHeader.Root>
@@ -18,26 +40,17 @@ export default function ProjectsPage() {
           <PageTitle>Recent runnings</PageTitle>
           <RecentExecutions.Root>
             <RecentExecutions.Item />
-            <RecentExecutions.Item />
-            <RecentExecutions.Item />
           </RecentExecutions.Root>
         </div>
 
         <div className="flex flex-col flex-1 gap-3">
           <PageTitle>Projects</PageTitle>
           <ProjectsGrid.Root>
-            <Link href="/projects/123">
-              <ProjectsGrid.Card />
-            </Link>
-            <Link href="/projects/123">
-              <ProjectsGrid.Card />
-            </Link>
-            <Link href="/projects/123">
-              <ProjectsGrid.Card />
-            </Link>
-            <Link href="/projects/123">
-              <ProjectsGrid.Card />
-            </Link>
+            {projects.map((project) => (
+              <Link key={project.id} href={`/projects/${project.id}`}>
+                <ProjectsGrid.Card project={project} />
+              </Link>
+            ))}
           </ProjectsGrid.Root>
         </div>
       </div>
--- webapp/components/recent-executions/item.tsx
+++ webapp/components/recent-executions/item.tsx
@@ -2,18 +2,18 @@ export function RecentExecutionsListItem() {
   return (
     <div className="bg-white rounded-sm border p-4 flex items-start gap-3">
       <div className="flex-1">
-        <div className="font-medium text-sm">Project name</div>
+        <div className="font-medium text-sm">Test</div>
         <div className="flex gap-2 mt-1">
           <span className="text-xs bg-gray-100 rounded px-2 py-0.5">
-            3 suites
+            x suites
           </span>
           <span className="text-xs bg-gray-100 rounded px-2 py-0.5">
-            18 test cases
+            xx test cases
           </span>
         </div>
       </div>
       <div className="flex flex-col items-end gap-1">
-        <span className="text-xs text-gray-400">2d ago</span>
+        <span className="text-xs text-gray-400">1d ago</span>
       </div>
     </div>
   );
