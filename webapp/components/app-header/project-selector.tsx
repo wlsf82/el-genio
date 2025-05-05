ae9e0b29fda335d8cf927f4f73f38aeb1a0b3a41
Gabriel Neutzling <gabrielneutzling@gmail.com>
2025-05-04 20:36:20 -0400

    refactor project selector component

--- webapp/components/app-header/project-selector.tsx
+++ webapp/components/app-header/project-selector.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import * as React from "react";
+import { useRouter, useParams } from "next/navigation";
 import {
   Select,
   SelectContent,
@@ -7,21 +10,55 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { fetchProjects } from "@/services/projects";
+import { Project } from "@/types/projects";
 
 export function ProjectSelector() {
+  const router = useRouter();
+  const params = useParams();
+  const [projects, setProjects] = React.useState<Project[]>([]);
+  const [defaultValue, setDefaultValue] = React.useState<string>("");
+
+  React.useEffect(() => {
+    const projectId = params?.id as string;
+    if (projectId) {
+      setDefaultValue(projectId);
+    }
+  }, [params]);
+
+  React.useEffect(() => {
+    const loadProjects = async () => {
+      try {
+        const data = await fetchProjects();
+        setProjects(data);
+      } catch (error) {
+        console.error("Error loading projects:", error);
+      }
+    };
+
+    loadProjects();
+  }, []);
+
+  const handleProjectChange = (projectId: string) => {
+    router.push(`/projects/${projectId}`);
+  };
+
   return (
-    <Select>
+    <Select value={defaultValue} onValueChange={handleProjectChange}>
       <SelectTrigger className="text-xs uppercase h-auto border-none shadow-none focus-visible:ring-0 px-0">
         <SelectValue placeholder="Select a project" />
       </SelectTrigger>
       <SelectContent alignOffset={-12} position="popper">
         <SelectGroup>
-          <SelectItem className="uppercase" value="tat">
-            Talking about testing
-          </SelectItem>
-          <SelectItem className="uppercase" value="my_project">
-            My project
-          </SelectItem>
+          {projects.map((project) => (
+            <SelectItem
+              key={project.id}
+              className="uppercase"
+              value={project.id}
+            >
+              {project.name}
+            </SelectItem>
+          ))}
         </SelectGroup>
       </SelectContent>
     </Select>
