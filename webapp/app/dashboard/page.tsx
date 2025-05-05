06cbe6bd85825f3a901d2879e957013c2da103a1
Gabriel Neutzling <gabrielneutzling@gmail.com>
2025-05-04 20:43:00 -0400

    add redirects

--- webapp/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex flex-col h-screen">
-      <h1>dashboard</h1>
-    </div>
-  );
-}
--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/projects",
+        permanent: true,
+      },
+      {
+        source: "/projects/:id",
+        destination: "/projects/:id/suites",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
